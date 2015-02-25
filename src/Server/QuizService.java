d92b5d5a9c32f36f47964bc2e5b48b577207df0c
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-02-25 23:18:13 +0000

    Added new method to create a new quiz

--- src/Server/QuizService.java
+++ src/Server/QuizService.java
@@ -1,7 +1,18 @@
 package Server;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
 /**
  * Created by Ahmed on 2/25/2015.
  */
-public interface QuizService {
+public interface QuizService extends Remote{
+    /**
+     *
+     * Creates a new quiz on the server and returns the quiz ID
+     * @param quizName The name of the quiz
+     * @param questions An arbitrary number of questions in which the quiz will contain
+     * @return The ID of the newly created quiz
+     */
+    public int newQuiz(String quizName, Question... questions) throws RemoteException;
 }
