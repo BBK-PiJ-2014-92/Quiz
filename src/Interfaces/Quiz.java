9055305e51bf86958fe6f7459bdfcc23564a8ced
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-02-25 23:45:39 +0000

    Added a getter method for ID and Name

--- src/Interfaces/Quiz.java
+++ src/Interfaces/Quiz.java
@@ -4,4 +4,16 @@ package Interfaces;
  * Created by Ahmed on 2/25/2015.
  */
 public interface Quiz {
+    /**
+     * Returns the ID of the quiz
+     *
+     * @return the ID of the quiz
+     */
+    public int getID();
+    /**
+     * Returns the name of the quiz
+     *
+     * @return the name of the quiz
+     */
+    public int getName();
 }
