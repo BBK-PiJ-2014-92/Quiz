2791781bbcb5b41be49e4947067e0ade5fcba44a
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-02-26 02:29:58 +0000

    Created basic outline test for QuizService

---
+++ src/Tests/QuizServiceTest.java
@@ -0,0 +1,42 @@
+package Tests;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class QuizServiceTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testNewQuiz() {
+
+    }
+
+    @Test
+    public void testCloseQuiz()  {
+
+    }
+
+    @Test
+    public void testPlayQuiz()  {
+
+    }
+
+    @Test
+    public void testCurrentQuizzes()  {
+
+    }
+
+    @Test
+    public void testIsCorrectAnswer()  {
+
+    }
+
+    @Test
+    public void testTotalScore()  {
+
+    }
+}
