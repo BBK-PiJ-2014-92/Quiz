31bc4a0ce48e9bc12aae3606cfe1cf49a7265f72
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-02-26 01:48:51 +0000

    Created test file for testing the Quiz

---
+++ src/Tests/QuizTest.java
@@ -0,0 +1,26 @@
+package Tests;
+
+import org.junit.Test;
+
+public class QuizTest {
+
+    @Test
+    public void testAddQuestions() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPlayers() throws Exception {
+
+    }
+
+    @Test
+    public void testAddPlayer() throws Exception {
+
+    }
+
+    @Test
+    public void testGetHighScores() throws Exception {
+
+    }
+}
