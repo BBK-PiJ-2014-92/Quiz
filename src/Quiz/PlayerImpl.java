43833dad13724a29ab7110a687b5703dc33bf1c9
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-02-26 04:27:00 +0000

    Implemented getter and setter as well as constructor

--- src/Quiz/PlayerImpl.java
+++ src/Quiz/PlayerImpl.java
@@ -8,4 +8,21 @@ public class PlayerImpl {
     private static int IDCOUNT = 0;
     private int id;
 
+    public PlayerImpl(String name) {
+        this.name =  name;
+        IDCOUNT++;
+        this.id = IDCOUNT;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
