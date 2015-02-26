420bb81303427cbdac0a2b2e31dad1860599c2bc
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-02-26 01:41:50 +0000

    Added all the getters and setters for the possible field members

--- src/Interfaces/Question.java
+++ src/Interfaces/Question.java
@@ -1,5 +1,7 @@
 package Interfaces;
 
+import java.util.List;
+
 /**
  * Created by Ahmed on 2/26/2015.
  */
@@ -10,4 +12,32 @@ public interface Question {
      * @return the name of the question
      */
     public String getQuestionName();
+    /**
+     * Sets the name of the question
+     * @param questionName the name of the question to be set
+     */
+    public void setQuestionName(String questionName);
+    /**
+     * Returns the correct answer of the question
+     *
+     * @return the correct answer of the question
+     */
+    public String getCorrectAnswer();
+    /**
+     * Sets the answer for the question
+     * @param answer the answer for the question
+     */
+    public void setCorrectAnswer(String answer);
+    /**
+     * Returns a list of possible answers for the question (not including the actual answer)
+     *
+     * @return a list of possible answers for the question
+     */
+    public List<String> getPossibleAnswers();
+    /**
+     * Replaces the list of possible answer with another list of possible answers (not including the actual answer)
+     * @param newPossibleAnswers the new list of possible answers to replace the existing list
+     */
+    public void setPossibleAnswers(List<String> newPossibleAnswers);
+
 }
