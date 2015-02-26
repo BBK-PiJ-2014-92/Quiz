aa2bfd4ebdddb5d8a780d5c2540a7d0245e8eff0
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-02-26 02:29:40 +0000

    Created basic outline tests for Player

---
+++ src/Tests/PlayerTest.java
@@ -0,0 +1,16 @@
+package Tests;
+
+import org.junit.Test;
+
+public class PlayerTest {
+
+    @Test
+    public void testGetQuizzes() throws Exception {
+
+    }
+
+    @Test
+    public void testAddScore() throws Exception {
+
+    }
+}
