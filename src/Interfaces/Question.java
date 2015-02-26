2599be390a539e51bc782224a6ac73b743704a9b
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-02-26 01:21:44 +0000

    Created a new interface for questions to implement. Added getter for question name

---
+++ src/Interfaces/Question.java
@@ -0,0 +1,13 @@
+package Interfaces;
+
+/**
+ * Created by Ahmed on 2/26/2015.
+ */
+public interface Question {
+    /**
+     * Returns the name of the question
+     *
+     * @return the name of the question
+     */
+    public String getQuestionName();
+}
