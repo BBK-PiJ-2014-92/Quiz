3854f512186cebedb815ddb932382a8f8feec0b4
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-02-26 04:15:08 +0000

    Added a buildUp

--- src/Tests/PlayerTest.java
+++ src/Tests/PlayerTest.java
@@ -1,16 +1,37 @@
 package Tests;
 
+import Interfaces.Player;
+import Interfaces.Question;
+import Interfaces.Quiz;
+import Quiz.PlayerImpl;
+import Quiz.QuestionImpl;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerTest {
+    private Player p1;
+    private Quiz quiz = new QuizImpl("The art of Muda", null, null);
+
+    @Before
+    public void buildUp(){
+        p1 = new PlayerImpl("Dio Brando");
+        List<String> possibleAnswers = new ArrayList<String>();
+        possibleAnswers.add("ORA ORA ORA ORA!");
+        possibleAnswers.add("RERO RERO RERO RERO!");
+        Question question = new QuestionImpl("Can a monkey fight a man?", "MUDA MUDA MUDA MUDA!", possibleAnswers);
+        quiz.setQuestions(question);
+    }
 
     @Test
-    public void testGetQuizzes() throws Exception {
+    public void testGetQuizzes() {
 
     }
 
     @Test
-    public void testAddScore() throws Exception {
+    public void testAddScore() {
 
     }
 }
