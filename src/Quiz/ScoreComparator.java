2a6165402bb0413d959873467d63d29e87bda81e
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-02-27 17:22:42 +0000

    Created a class to allow comparing of scores obtained by different players

---
+++ src/Quiz/ScoreComparator.java
@@ -0,0 +1,17 @@
+package Quiz;
+
+import Interfaces.Player;
+
+import java.util.Comparator;
+
+/**
+ * Created by Ahmed on 2/27/2015.
+ */
+public class ScoreComparator implements Comparator<Player> {
+
+
+    @Override
+    public int compare(Player o1, Player o2) {
+        return 0;
+    }
+}
