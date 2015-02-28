0d1530961f19428f02711cfc8d72a228e9beae08
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-02-28 00:03:18 +0000

    Created server class which implements the service interface

---
+++ src/Server/QuizServer.java
@@ -0,0 +1,9 @@
+package Server;
+
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ * Created by Ahmed on 2/28/2015.
+ */
+public class QuizServer extends UnicastRemoteObject implements QuizService {
+}
