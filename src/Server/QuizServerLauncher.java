4d945693dfa6bbac759b0c42cb779952cf8e7b98
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-02-28 23:49:47 +0000

    Created a launcher for the server

---
+++ src/Server/QuizServerLauncher.java
@@ -0,0 +1,31 @@
+package Server;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+
+/**
+ * Created by Ahmed on 2/28/2015.
+ */
+public class QuizServerLauncher {
+    public static void main(String[] args) {
+        QuizServerLauncher launcher = new QuizServerLauncher();
+        launcher.launch();
+    }
+
+    private void launch() {
+        try {
+            LocateRegistry.createRegistry(1099);
+            QuizServer server = new QuizServer();
+            Naming.rebind("quiz", server);
+            System.out.println("Connecting to the QuizServer");
+        } catch (RemoteException e) {
+            e.printStackTrace();
+            System.out.println("Failed to connect to the QuizServer");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            System.out.println("Could not find server");
+        }
+    }
+}
