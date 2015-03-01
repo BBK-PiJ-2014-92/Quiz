70e8c64a7bd89ab4ddffc9ba135e269d9135b8e8
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-03-01 03:36:23 +0000

    Created setup client class and created a method serverConnection() which connects the client to the server

---
+++ src/Client/SetupClient.java
@@ -0,0 +1,29 @@
+package Client;
+
+import Server.QuizService;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+/**
+ * Created by Ahmed on 3/1/2015.
+ */
+public class SetupClient {
+    private QuizService server;
+
+    public void serverConnection() {
+        Registry registry = null;
+        try {
+            registry = LocateRegistry.getRegistry(1099);
+            server = (QuizService) registry.lookup("quiz");
+            System.out.println("Connected to QuizServer");
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
