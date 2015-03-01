321e8a06a31e6ffa7f67535f0aa500ae331dd00c
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-03-01 21:23:18 +0000

    Implemented launch

--- src/Client/ClientLauncher.java
+++ src/Client/ClientLauncher.java
@@ -1,6 +1,7 @@
 package Client;
 
 import java.rmi.RemoteException;
+import java.util.Scanner;
 
 /**
  * Created by Ahmed on 3/1/2015.
@@ -11,7 +12,38 @@ public class ClientLauncher {
         launcher.launch();
     }
 
-    public void launch() {
+    public void launch() throws RemoteException {
+        System.out.println("Welcome to Quiz Server!");
+        boolean finished = false;
+        while (!finished) {
+            System.out.println("Please select one of the following: ");
+            System.out.println("1. Setup Quiz");
+            System.out.println("2. Play Quiz");
+            System.out.println("3. Quit");
+            Scanner sc = new Scanner(System.in);
+            boolean isInt = sc.hasNextInt();
+            while (!isInt) {
+                System.out.println("Please enter a number");
+                sc.nextLine();
+                isInt = sc.hasNextInt();
+            }
+            int number = Integer.parseInt(sc.nextLine());
+            switch (number) {
+                case 1:
+                    new SetupClient().launch();
+                    break;
+                case 2:
+                    new PlayerClient().launch();
+                    break;
+                case 3:
+                    System.out.println("Thanks for playing");
+                    finished = true;
+                    break;
+                default:
+                    System.out.println("Please enter one of the available options");
+                    break;
+            }
 
+        }
     }
 }
