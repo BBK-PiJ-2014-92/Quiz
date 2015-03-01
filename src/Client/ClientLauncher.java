e1412baeb93fdb0a818abc26dffae78fbdef3a5f
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-03-01 21:18:33 +0000

    Created a ClientLauncher class

---
+++ src/Client/ClientLauncher.java
@@ -0,0 +1,17 @@
+package Client;
+
+import java.rmi.RemoteException;
+
+/**
+ * Created by Ahmed on 3/1/2015.
+ */
+public class ClientLauncher {
+    public static void main(String[] args) throws RemoteException {
+        ClientLauncher launcher = new ClientLauncher();
+        launcher.launch();
+    }
+
+    public void launch() {
+
+    }
+}
